221619fbcac7b5a37db1369232422fd105cf340e
divs94 <divyatlng0@gmail.com>
2022-11-22 17:15:17 +0530

    fixed alignment for about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function About() {
   return (
     <>
-     <div className="container mx-auto">
+     <div className="container mx-auto about_section">
        <div className="container d-flex flex-row justify-content-center mt-5 pt-5">
         <div className="row">
           <div className="col-md-8 d-flex flex-column justify-content-center">
@@ -24,18 +24,20 @@ export default function About() {
               most customer-centric company, Earth’s best employer, and Earth’s
               safest place to work.
             </p>
-            <NavLink
+           <div className="contact_btn">
+           <NavLink
               to="/contact"
-              className="btn btn-outline-dark text-center mt-2 display-6"
+              className="btn btn-outline-dark text-center mt-2 display-6 contact_btn"
               style={{ width: "400px", height: "50px" }}
             >
             
               <i class="fa fa-envelope me-2" aria-hidden="true"></i>
               Contact Us
             </NavLink>
+           </div>
           </div>
         </div>
-        <div className="col-md-4 mt-2 about_image">
+        <div className="col-md-6 mt-2 about_image">
           <img src="/assets/about2.jpg" className="ms-2 mt-2" width="400px" height="250px" alt="" />
           <img src="/assets/about21.webp" className="ms-5 mt-2" width="450px" height="200px" alt="" />
           <img src="/assets/about.jpg" className="me-4 mt-2" width="350px" height="200px" alt="" />
